f2f5542f4d2cf401c00396af9b560ba333fffd38
franco fallatti <francofallatti@gmail.com>
2024-01-14 11:04:49 -0300

    add activity 4

---
+++ Guia de actividades Nro 1. OpenMP/4.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<omp.h>
+
+int main() {
+
+    int i=0;  // Declarar i fuera de la región paralela
+
+    #pragma omp parallel
+    {
+        printf("Hello World desde el hilo con el id: %d\n", omp_get_thread_num());
+
+        while(i < 10) {
+            printf("Iteration: %d from thread %d\n", i, omp_get_thread_num());
+            i++;
+        }
+    }
+
+    printf("Bye\n");
+    return 0;
+}
