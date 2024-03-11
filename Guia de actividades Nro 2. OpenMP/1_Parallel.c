34c331ba18991c06902053aef64307316ac9174e
unknown <francofallatti@gmail.com>
2024-03-11 12:41:21 -0300

    add 1 parallel

---
+++ Guia de actividades Nro 2. OpenMP/1_Parallel.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <omp.h>
+
+int buscaMaximo(int *a, int desde, int hasta) {
+    int max_local = a[desde]; // Inicializa max_local con el primer elemento del sub-arreglo
+    int i;
+
+    // OpenMP paraleliza solo el bucle for
+    #pragma omp parallel reduction(max:max_local) num_threads(2) 
+    {
+        int tid = omp_get_thread_num();
+        #pragma omp for 
+        for (i = desde + 1; i < hasta; i++) {
+            if (a[i] > max_local) {
+                max_local = a[i];
+            }
+        }
+        printf("Maximun in theread %d is: %d\n", tid, max_local);
+    }
+    
+    
+    return max_local;
+}
+
+int main() {
+    int arreglo[] = {1, 5, 9, 3, 7, 2, 8, 6, 4};
+    int desde = 0;
+    int hasta = sizeof(arreglo) / sizeof(arreglo[0]);
+
+    int maximo = buscaMaximo(arreglo, desde, hasta);
+    printf("The maximum is: %d\n", maximo);
+
+    return 0;
+}
